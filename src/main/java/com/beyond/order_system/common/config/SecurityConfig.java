93afe4813d6343a961e7fac4b5d8f6c3fda977d4
이지연 <wldus019113@icloud.com>
2026-03-06 13:15:44 +0900

    CORS 내 프론트 도메인 추가

--- src/main/java/com/beyond/order_system/common/config/SecurityConfig.java
+++ src/main/java/com/beyond/order_system/common/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     /* *********************** CORS 처리 *********************** */
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://www.eazy99.shop"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
